5043c8e539e0da29b3bc2f5975f6bc19e4012e49
Alexander Kiel <alexanderkiel@gmx.net>
2012-01-28 17:19:32 +0100

    initial commit

---
+++ project.clj
@@ -0,0 +1,3 @@
+(defproject kata-bank-ocr "0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.2.1"]])
---
+++ test/kata_bank_ocr/test/core.clj
@@ -0,0 +1,6 @@
+(ns kata-bank-ocr.test.core
+  (:use [kata-bank-ocr.core])
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
